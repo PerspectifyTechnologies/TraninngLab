aa5a863b55fc0cdfa80a14ae277baecdab436caa
Pancham <panchamb63@gmail.com>
2021-06-18 16:18:03 +0530

    Did some minor changes to signUp page

--- Front-end/sample-app/src/App.js
+++ Front-end/sample-app/src/App.js
@@ -11,10 +11,10 @@ function App() {
 
   return (
     <>
-      {/* <FrontPage />
-      <EventPage />
-      <CoursePage />
-      <TestPage /> */}
+      {/* <FrontPage /> */}
+      {/* <EventPage /> */}
+      {/* <CoursePage /> */}
+      {/* <TestPage /> */}
       {/* <SignUp /> */}
     </>
   )
