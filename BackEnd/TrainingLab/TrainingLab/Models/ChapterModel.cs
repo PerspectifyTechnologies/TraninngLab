cb4b29def46fea195d720e29018ca80cc10faa05
Himani Patel <62655342+Himanipatell@users.noreply.github.com>
2021-06-19 12:49:28 +0530

    Backend Added.

---
+++ BackEnd/TrainingLab/TrainingLab/Models/ChapterModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingLab.Models
+{
+    public class ChapterModel
+    {
+        public int Id { get; set; }
+        public string ChapterName { get; set; }
+        public List<string> Topics { get; set; }
+        public string VideoURL { get; set; }
+        public string NotesURL { get; set; }
+    }
+}
---
+++ BackEnd/TrainingLab/TrainingLab/Models/CourseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingLab.Models
+{
+    public class CourseModel
+    {
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public List<string> Chapters { get; set; }
+    }
+}
