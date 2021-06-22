5442b6a457918939e05fecdfd2a51341d5a8defc
Abhinandan14 <Abhinandan.pun@gmail.com>
2021-06-22 12:12:21 +0530

    updated registermodel

--- Backend/WebApi/WebApi/AuthenticationServices/RegisterServices/RegisterModel.cs
+++ Backend/WebApi/WebApi/AuthenticationServices/RegisterServices/RegisterModel.cs
@@ -5,20 +5,9 @@ namespace WebApi.DatabaseServices
     public class RegisterModel
     {
 
-        [Required(ErrorMessage = "User Name is required")]
         public string UserName { get; set; }
-
-        [Required(ErrorMessage = "First name is required")]
-        public string FirstName { get; set; }
-
-        [Required(ErrorMessage = "Last name is required")]
-        public string LastName { get; set; }
-
-        [Required(ErrorMessage = "Password is required")]
+        public string Email { get; set; }
         public string Password { get; set; }
 
-        [EmailAddress]
-        [Required(ErrorMessage = "Email address is required")]
-        public string Email { get; set; }
     }
 }
