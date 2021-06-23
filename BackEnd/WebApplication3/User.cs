e2fe4e305a28d792305171996fd73c987f34390a
2001ritu <66665883+2001ritu@users.noreply.github.com>
2021-06-23 22:57:30 +0530

    login\signup

---
+++ BackEnd/WebApplication3/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace WebApplication3
+{
+    public class User
+    {
+        public string name { get; set; }
+        //public string id { get; set; }
+        public string email { get; set; }
+      //  public decimal contact { get; set; }
+       // public string address { get; set; }
+        public string password { get; set; }
+    }
+}
+
