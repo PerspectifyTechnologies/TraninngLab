8240481fdfd5aa30877acdfa075e32867f31ad00
Himani Patel <62655342+Himanipatell@users.noreply.github.com>
2021-06-25 10:35:53 +0530

    Update ChapterModel.cs

--- BackEnd/TrainingLab/TrainingLab/Models/ChapterModel.cs
+++ BackEnd/TrainingLab/TrainingLab/Models/ChapterModel.cs
@@ -8,9 +8,8 @@ namespace TrainingLab.Models
     public class ChapterModel
     {
         public int Id { get; set; }
-        public string ChapterName { get; set; }
+        
         public List<string> Topics { get; set; }
-        public string VideoURL { get; set; }
-        public string NotesURL { get; set; }
+        
     }
 }
