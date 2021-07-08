eb534a725aed3a5f7a602943aa189b446792672f
Abhinandan14 <Abhinandan.pun@gmail.com>
2021-07-08 16:03:17 +0530

    update on new sql file

--- Backend/AwsSnapShot.sql
+++ Backend/AwsSnapShot.sql
@@ -346,8 +346,8 @@ DROP TABLE IF EXISTS `refreshtokens`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `refreshtokens` (
-  `username` varchar(258) NOT NULL,
-  `refreshtoken` datetime NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `refreshtoken` varchar(258) NOT NULL,
   `expirationdate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
